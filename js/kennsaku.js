a9700f75a378f131d58ee5d10e99923d32909156
hoshinoshiho999-jpg <hoshinoshiho999@gmail.com>
2026-02-27 16:18:47 +0900

    Add files via upload

---
+++ js/kennsaku.js
@@ -0,0 +1,23 @@
+
+            var element = document.form1.text1.value;
+            if(element =="1"){
+                 alert("1");
+                 location.href = "https://crocro.com/";
+            }
+            else if(element =="2"){
+                alert("2");
+                location.href = "https://crocro.com/";
+            }
+            else if(element =="3"){
+                alert("3");
+                location.href = "https://crocro.com/";
+            }
+            else if(element =="ok"){
+                alert("不正解");
+            }
+            else if(element =="ok"){
+                alert("不正解");
+            }
+            else if(element =="ok"){
+                alert("不正解");
+            }
